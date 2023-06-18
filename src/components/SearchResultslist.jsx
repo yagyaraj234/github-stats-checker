aa5a40ca27d3c1b1b04cb810f9e74860fe08919e
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-19 01:03:47 +0530

    component in not used

---
+++ src/components/SearchResultslist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { SearchResult } from './SearchResult';
+const SearchResultslist = ({results}) => {
+  return (
+    <div className='results-list '>
+         
+     
+
+    </div>
+  )
+}
+
+export default SearchResultslist
