43477f5537a8b9a046e95d2f7160c18a329dfd06
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-19 01:02:43 +0530

    component called

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+
 import "./App.css";
-import Home from "./components/Home";
+import Search from "./components/Home";
 
 function App() {
 
-  return <div className="bg-[#F1F1F1] text-[#413E4E] flex items-center justify-center min-w-screen min-h-screen relative ">
-    
-    <Home/>
-    
-  </div>;
+  return (
+    <div className="bg-[#F1F1F1] text-[#413E4E] flex flex-col items-center min-w-screen min-h-screen relative  ">
+      <Search  />
+    </div>
+  );
 }
 
 export default App;
