704d76ea621d8a211e9bbcfa8665a7cadf0a13f6
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-19 21:00:23 +0530

    esponsive

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
 import RepoList from "./RepoList";
-import axios from "axios";
 
-export const SearchResult = ({ data }) => {
-  const [repoData, setRepoData] = useState([]);
-
-  useEffect(() => {
-    fetchRepo();
-  }, [data]);
-  const fetchRepo = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.github.com/users/${data}/repos?sort=created`
-      );
-      const repos = response.data;
-
-      if (repos.length > 0) {
-        const latestRepo = repos[0];
-        setRepoData(latestRepo);
-      }
-    } catch (error) {
-      console.error("Error fetching repository:", error);
-    }
-  };
+export const SearchResult = ({ data, username }) => {
   return (
-    <div className="mx-auto flex flex-col items-center  my-10 bg-white rounded-md  border-2">
-      {/* First Section Start  */}
+    <div className="mx-auto  flex flex-col items-center  my-10 bg-white rounded-md  border-2">
+      {/* Icon Section Start  */}
       <div className="flex flex-row justify-center gap-2  py-2 md:px-3">
         <div className="text-center md:w-3/12  w-1/3">
           <img src={data.avatar_url} alt="text" className=" rounded-md" />
@@ -46,45 +24,55 @@ export const SearchResult = ({ data }) => {
               Following :{data.following}
             </p>
           </div>
-          <div className="w-full md:my-1 my-2 border rounded border-gray-300">
-            <h3 className=" flex font-bold border-b  md:p-2 p-1 pl-0 ">
-              Company :{" "}
+
+          {/* Icon section End  */}
+
+          {/* User Details Start  */}
+
+          <div className="w-full md:my-1 my-2 border rounded border-gray-300 px-2 text-xs md:text-lg">
+            <h3 className=" flex font-bold border-b  md:p-2 py-1 pl-0 items-center ">
+              Company :
               <span className="  font-normal">
-                {" "}
-                {data.company === null && (
-                  <p className="text-sm font-semibold px-2">NULL</p>
+                {data.company === null ? (
+                  <p className="text-xs font-semibold px-2 py-1">NULL</p>
+                ) : (
+                  <p className="px-1">{data.company}</p>
                 )}
               </span>
             </h3>
-            <h3 className="font-bold flex border-b md:p-2 p-1 pl-0 ">
+            <h3 className="flex font-bold border-b  md:p-2 py-1 pl-0 items-center">
               Website :{" "}
               <span className="font-normal">
-                {" "}
-                {data.website === null && (
-                  <p className="text-sm font-semibold px-2">NULL</p>
+                {data.website === null ? (
+                  <p className="text-sm font-semibold px-2 py-1">NULL</p>
+                ) : (
+                  <p className="px-1">{data.website}</p>
                 )}
               </span>
             </h3>
-            <h3 className="font-bold flex border-b md:p-2 p-1 pl-0 ">
-              Location : <span className="font-normal"> {data.location}</span>
+            <h3 className="font-bold flex border-b md:p-2 py-1 pl-0 ">
+              Location : <span className="font-normal px-1"> {data.location}</span>
             </h3>
             <h3 className="font-bold md:p-2 p-1 pl-0 hid">
               Member Since :{" "}
-              <span className="font-normal"> {Date(data.created_at)}</span>
+              <span className="font-normal"> {Date(data.created_at).slice(0,15)}</span>
             </h3>
           </div>
         </div>
       </div>
+       {/* User Details End  */}
 
       {/* First Section End  */}
 
       {/* repo section start  */}
-      <RepoList repoData={repoData} />
+
+      <RepoList username={username} />
+
       {/* repo section end */}
 
       {/* bottom Section  Start  */}
-      <div className="ml-5">
-        <button className="bg-blue-600 text-white md:p-2 p-1 rounded-md ">
+      <div className="ml-5 mb-5">
+        <button className="bg-blue-600 text-white md:p-2 p-1 px-4 rounded-md hover:bg-blue-500  ">
           <a href={data.html_url} target="_blank" rel="noreferrer">
             View Full Profile
           </a>
