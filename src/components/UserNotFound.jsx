568f99b13f552486b1fe035e36153f42f2583af9
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-19 21:01:04 +0530

    final commit 🔥🔥

--- src/components/UserNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserNotFound = ({username}) => {
-  return (
-    <div className='p-5 mx-auto text-4xl my-10 opacity-80'> User  Not Found in DataBase </div>
-  )
-}
-
-export default UserNotFound
