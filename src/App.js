0b561163c87684d4a53c5a35661b194f4b01e002
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-06-19 21:00:38 +0530

    wrap up 🔥🔥

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   return (
     <div className="bg-[#F1F1F1] text-[#413E4E] flex flex-col items-center min-w-screen min-h-screen relative  ">
       <Search  />
+
+      <div className=" flex flex-row  gap-2 absolute bottom-0 w-full justify-center items-center py-2 border-t-2">
+        <p>Created by Yagyaraj</p>
+          <a href="https://github.com/yagyaraj234" target="_blank" rel='noreferrer'>
+          <ion-icon name="logo-github"></ion-icon>
+          </a>
+      </div>
     </div>
   );
 }
